44ac44e087a6f0159247a5ec43d783000936a790
Jasmine Mary <jasminemary630@gmail.com>
2023-09-30 22:26:00 +0530

    added tree programs

--- com/dev/tree/questions/BinaryTree.java
+++ com/dev/tree/questions/BinaryTree.java
@@ -29,25 +29,133 @@ public class BinaryTree {
 
     }
 
-    // public String serialize(TreeNode root) {
-        
-    // }
+    class Tuple {
+        Node node;
+        int row;
+        int col;
 
-    // // Decodes your encoded data to tree.
-    // public Node deserialize(String data) {
-        
-    // }
+        public Tuple(Node n, int r, int c) {
+            this.node = n;
+            this.row = r;
+            this.col = c;
+        }
+    }
+
+    public List<List<Integer>> verticalTraversal(Node root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> m = new TreeMap<>();
+
+        Queue<Tuple> q = new LinkedList<>();
+        q.add(new Tuple(root, 0, 0));
+
+        while (!q.isEmpty()) {
+            Tuple t = q.remove();
+            Node n = t.node;
+            int x = t.row;
+            int y = t.col;
+
+            if (!m.containsKey(x)) {
+                m.put(x, new TreeMap<>());
+            }
+
+            if (!m.get(x).containsKey(y)) {
+                m.get(x).put(y, new PriorityQueue<>());
+            }
+
+            m.get(x).get(y).add(n.value);
+
+            if (n.left != null) {
+                q.add(new Tuple(n.left, x - 1, y + 1));
+            }
+            if (n.right != null) {
+                q.add(new Tuple(n.right, x + 1, y + 1));
+            }
+        }
+
+        List<List<Integer>> l = new LinkedList<>();
+
+        for (TreeMap<Integer, PriorityQueue<Integer>> y : m.values()) {
+            l.add(new ArrayList<>());
+
+            for (PriorityQueue<Integer> v : y.values()) {
+                while (!v.isEmpty()) {
+                    l.get(l.size() - 1).add(v.poll());
+                }
+            }
+        }
+        return l;
+
+    }
+
+    // left -> root -> right
+    List<Integer> l2 = new ArrayList<>();
+
+    public List<Integer> inOrder(Node root) {
+        if (root == null) {
+            return l;
+        }
+        inOrder(root.left);
+        l.add(root.value);
+        inOrder(root.right);
+
+        return l;
+    }
+
+    // root -> left -> right
+    List<Integer> l = new ArrayList<>();
+
+    public List<Integer> preOrder(Node root) {
+        if (root == null) {
+            return l;
+        }
+        l.add(root.value);
+        preOrder(root.left);
+        preOrder(root.right);
+
+        return l;
+    }
+
+    // left -> right -> root
+    List<Integer> l1 = new ArrayList<>();
+
+    public List<Integer> postOrder(Node root) {
+        if (root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            l1.add(root.value);
+        }
+
+        return l1;
+    }
+
+    public List<Integer> preorderTraversal(Node root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<Node> s = new Stack<>();
+        s.push(root);
+
+        while (!s.empty()) {
+            Node cn = s.peek();
+            result.add(cn.value);
+            s.pop();
+            if (cn.left != null) {
+                s.push(cn.left);
+            }
+            if (cn.right != null) {
+                s.push(cn.right);
+            }
+        }
+        return result;
+    }
 
     public List<Integer> inorderTraversal(Node root) {
         List<Integer> result = new ArrayList<>();
         Stack<Node> s = new Stack<>();
 
-        while(true){
-            if(root != null){
+        while (true) {
+            if (root != null) {
                 s.push(root);
                 root = root.left;
-            }else{
-                if(s.empty()){
+            } else {
+                if (s.empty()) {
                     break;
                 }
                 root = s.peek();
@@ -61,39 +169,39 @@ public class BinaryTree {
 
     public boolean isSubtree(Node T, Node S) {
         // add code here.
-        if(S == null){
+        if (S == null) {
             return true;
         }
-        if(T == null){
+        if (T == null) {
             return false;
         }
 
-        if(areIdentical(T, S)){
+        if (areIdentical(T, S)) {
             return true;
         }
 
         return isSubtree(T.left, S) || isSubtree(T.right, S);
     }
 
-    public boolean areIdentical(Node t, Node s){
-        if(t == null && s == null){
+    public boolean areIdentical(Node t, Node s) {
+        if (t == null && s == null) {
             return true;
         }
-        if(t == null || s == null){
+        if (t == null || s == null) {
             return false;
         }
 
-        return (t.value == s.value) && areIdentical(t.left,s.left) && areIdentical(t.right,s.right);
+        return (t.value == s.value) && areIdentical(t.left, s.left) && areIdentical(t.right, s.right);
     }
 
-    //inorder successor of bt
-    public Node inorderSuccessor(Node root, Node n){
-        if(n.right != null){
+    // inorder successor of bst
+    public Node inorderSuccessor(Node root, Node n) {
+        if (n.right != null) {
             return minimum(n.right);
         }
 
         Node p = n.parent;
-        while(n.parent != null && n == p.right){
+        while (n.parent != null && n == p.right) {
             n = p;
             p = p.parent;
         }
@@ -102,23 +210,22 @@ public class BinaryTree {
     }
 
     private Node minimum(Node node) {
-        while (node.left != null){
+        while (node.left != null) {
             node = node.left;
         }
         return node;
     }
 
-
     public Node buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length == 0){
+        if (preorder.length == 0) {
             return null;
         }
 
         int root = preorder[0];
         int index = 0;
 
-        for(int i = 0; i < inorder.length; i++){
-            if(inorder[i] == root){
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == root) {
                 index = i;
             }
         }
@@ -127,7 +234,8 @@ public class BinaryTree {
 
         node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
 
-        node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length), Arrays.copyOfRange(inorder, index + 1, inorder.length));
+        node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length),
+                Arrays.copyOfRange(inorder, index + 1, inorder.length));
 
         return node;
     }
@@ -137,39 +245,40 @@ public class BinaryTree {
     }
 
     static int count = 0;
+
     public static Node kthSmallestHelper(Node root, int k) {
-        if(root == null){
+        if (root == null) {
             return null;
         }
 
         Node left = kthSmallestHelper(root.left, k);
-        if(left != null){
+        if (left != null) {
             return left;
         }
 
         count++;
 
-        if(count == k){
+        if (count == k) {
             return root;
         }
 
         return kthSmallestHelper(root.right, k);
-        
+
     }
 
     public static Node lowestCommonAncestor(Node root, Node p, Node q) {
-        if(root == null){
+        if (root == null) {
             return null;
         }
 
-        if(root == p || root == q){
+        if (root == p || root == q) {
             return root;
         }
 
         Node left = lowestCommonAncestor(root.left, p, q);
         Node right = lowestCommonAncestor(root.right, p, q);
 
-        if(left != null && right != null){
+        if (left != null && right != null) {
             return root;
         }
         return left == null ? right : left;
@@ -179,16 +288,15 @@ public class BinaryTree {
         return helper(root, null, null);
     }
 
-
-    public static boolean helper(Node node, Integer low, Integer high){
-        if(node == null){
+    public static boolean helper(Node node, Integer low, Integer high) {
+        if (node == null) {
             return true;
         }
 
-        if(low != null && node.value <= low){
+        if (low != null && node.value <= low) {
             return false;
         }
-        if(high != null && node.value >= high){
+        if (high != null && node.value >= high) {
             return false;
         }
 
@@ -199,15 +307,15 @@ public class BinaryTree {
     }
 
     public static void flatten(Node root) {
-        if(root == null){
+        if (root == null) {
             return;
         }
 
         Node cn = root;
-        while(cn != null){
-            if(cn.left != null){
+        while (cn != null) {
+            if (cn.left != null) {
                 Node temp = cn.left;
-                while(temp.right != null){
+                while (temp.right != null) {
                     temp = temp.right;
                 }
                 temp.right = cn.right;
@@ -220,9 +328,9 @@ public class BinaryTree {
     }
 
     public static int maxDepth(Node root) {
-        if(root == null){
+        if (root == null) {
             return 0;
-        }        
+        }
 
         int leftDep = maxDepth(root.left);
         int rightDep = maxDepth(root.right);
@@ -233,7 +341,7 @@ public class BinaryTree {
     }
 
     public static Node invertTree(Node root) {
-        if(root == null){
+        if (root == null) {
             return null;
         }
 
@@ -247,14 +355,15 @@ public class BinaryTree {
     }
 
     static int diameter = 0;
+
     public static int diameterOfBinaryTree(Node root) {
         height(root);
 
         return diameter - 1;
     }
 
-    public static int height(Node node){
-        if(node == null){
+    public static int height(Node node) {
+        if (node == null) {
             return 0;
         }
 
@@ -269,25 +378,25 @@ public class BinaryTree {
     }
 
     public static boolean isSymmetric(Node root) {
-        if(root == null){
+        if (root == null) {
             return false;
         }
         Queue<Node> q = new LinkedList<>();
         q.offer(root.left);
         q.offer(root.right);
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Node left = q.poll();
             Node right = q.poll();
 
-            if(left == null && right == null){
+            if (left == null && right == null) {
                 continue;
             }
-            if(left == null || right == null){
+            if (left == null || right == null) {
                 return false;
             }
 
-            if(left.value != right.value){
+            if (left.value != right.value) {
                 return false;
             }
             q.add(left.left);
@@ -299,76 +408,73 @@ public class BinaryTree {
         return true;
     }
 
-    public static boolean isCousin(Node root, int x, int y){
+    public static boolean isCousin(Node root, int x, int y) {
         Node xx = findNode(root, x);
         Node yy = findNode(root, y);
 
         return (level(root, xx, 0) == level(root, yy, 0) && (!isSibling(root, xx, yy)));
     }
 
-    public static Node findNode(Node node, int x){
-        if(node == null){
+    public static Node findNode(Node node, int x) {
+        if (node == null) {
             return null;
         }
-        if(node.value == x){
+        if (node.value == x) {
             return node;
         }
         Node n = findNode(node.left, x);
-        if(n != null){
+        if (n != null) {
             return n;
         }
         return findNode(node.right, x);
     }
 
-    public static boolean isSibling(Node node, Node x, Node y){
-        if(node == null){
+    public static boolean isSibling(Node node, Node x, Node y) {
+        if (node == null) {
             return false;
         }
-        return (
-        (node.left == x && node.right == y) || 
-        (node.left == y && node.right == x) || 
-        isSibling(node.left, x, y) || 
-        isSibling(node.right, x, y)
-        );
+        return ((node.left == x && node.right == y) ||
+                (node.left == y && node.right == x) ||
+                isSibling(node.left, x, y) ||
+                isSibling(node.right, x, y));
     }
 
-    public static int level(Node node, Node x, int lev){
-        if(node == null){
+    public static int level(Node node, Node x, int lev) {
+        if (node == null) {
             return 0;
         }
 
-        if(node == x){
+        if (node == x) {
             return lev;
         }
         int l = level(node.left, x, lev + 1);
-        if(l != 0){
+        if (l != 0) {
             return l;
         }
-        return level(node.right, x, lev+1);
+        return level(node.right, x, lev + 1);
     }
 
-
-    public static List<Integer> rightSideView(Node root){
+    public static List<Integer> rightSideView(Node root) {
         List<Integer> list = new ArrayList<>();
 
-        if(root == null){
+        if (root == null) {
             return list;
         }
 
         Queue<Node> q = new LinkedList<>();
         q.offer(root);
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int level = q.size();
-            for(int i = 0; i < level; i++){
+            for (int i = 0; i < level; i++) {
                 Node cn = q.poll();
-                if(i == level - 1){
+                if (i == level - 1) {
                     list.add(cn.value);
                 }
-                if(cn.left != null){
+                if (cn.left != null) {
                     q.offer(cn.left);
                 }
-                if(cn.right != null){
+                if (cn.right != null) {
                     q.offer(cn.right);
                 }
             }
@@ -377,20 +483,20 @@ public class BinaryTree {
     }
 
     public static Node connect(Node root) {
-        if(root == null){
+        if (root == null) {
             return null;
         }
 
         Node leftMost = root;
 
-        while(leftMost.left != null){
+        while (leftMost.left != null) {
             Node cn = leftMost;
 
-            while(cn != null){
+            while (cn != null) {
                 if (cn.left != null) {
                     cn.left.next = cn.right;
                 }
-                
+
                 if (cn.right != null && cn.next != null) {
                     cn.right.next = cn.next.left;
                 }
